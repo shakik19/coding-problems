e3ac898b80664aa288976baf7da476782479654c
shakik19 <1ne9ine.o@gmail.com>
2023-04-17 13:02:53 +0600

    update

--- ICS/loop/ten.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main() {
-  int n, sum = 0;
-  scanf("%i", &n);
-  for (int i = 1; i <= n; i++) {
-    (n % 2 == 0) ? (sum += n * (-1)) : (sum += n);
-  }
-  printf("%i", sum);
-}
